e01a2a5aa019dc49e0f5f4fe61249afb6d452d72
Matt Bertrand <matt@epidemico.com>
2016-03-11 16:04:58 -0500

    Update ansible scripts to include HIFLD datasets and celery tasks.  Also updated celery schedule for all tasks.

--- ansible/roles/dataqs/templates/dataq_settings.py
+++ ansible/roles/dataqs/templates/dataq_settings.py
@@ -32,7 +32,8 @@ DATAQS_APPS = (
     'dataqs.nasa_gpm',
     'dataqs.spei',
     'dataqs.usgs_quakes',
-    'dataqs.wqp'
+    'dataqs.wqp',
+    'dataqs.hifld',
 )
 
 # CELERY SETTINGS
@@ -73,51 +74,94 @@ RSYNC_WAIT_TIME = 0
 CELERYBEAT_SCHEDULE = {
     'gfms': {
         'task': 'dataqs.gfms.tasks.gfms_task',
-        'schedule': crontab(minute='3'),
+        'schedule': crontab(minute=3),
         'args': ()
     },
     'forecast_io': {
         'task': 'dataqs.forecastio.tasks.forecast_io_task',
-        'schedule': crontab(minute='1'),
+        'schedule': crontab(minute=1),
         'args': ()
     },
     'aqicn': {
         'task': 'dataqs.aqicn.tasks.aqicn_task',
-        'schedule': crontab(hour='5'),
+        'schedule': crontab(hour=5, minute=0),
         'args': ()
     },
     'gdacs': {
         'task': 'dataqs.gdacs.tasks.gdacs_task',
-        'schedule': crontab(hour='1'),
+        'schedule': crontab(hour=1, minute=0),
         'args': ()
     },
     'usgs_quakes': {
         'task': 'dataqs.usgs_quakes.tasks.usgs_quake_task',
-        'schedule': crontab(hour='3'),
+        'schedule': crontab(hour=3,  minute=0),
         'args': ()
     },
     'spei': {
         'task': 'dataqs.spei.tasks.spei_task',
-        'schedule': crontab(hour='4'),
+        'schedule': crontab(hour=4, minute=0),
         'args': ()
     },
     'wqp': {
         'task': 'dataqs.wqp.tasks.wqp_task',
-        'schedule': crontab(hour='2'),
+        'schedule': crontab(hour=2, minute=0),
         'args': ()
     },
     'airnow': {
         'task': 'dataqs.gfms.tasks.airnow_task',
-        'schedule': crontab(hour='2'),
+        'schedule': crontab(hour=6, minute=0),
         'args': ()
     },
     'nasa_gpm': {
         'task': 'dataqs.nasa_gpm.tasks.nasa_gpm_task',
-        'schedule': crontab(hour='2'),
+        'schedule': crontab(hour=7, minute=0),
+        'args': ()
+    },
+    'hifld': {
+        'task': 'dataqs.hifld.tasks.hifld_task',
+        'schedule': crontab(day_of_week='sunday', hour=12, minute=0),
         'args': ()
     }
 }
 
+HIFLD_LAYERS = [
+    {
+        'name': 'US State Boundaries',
+        'url': 'https://hifld-dhs-gii.opendata.arcgis.com/datasets/718791120f6549708cb642dac6ff0dbf_0.geojson',
+        'table': 'us_state_boundaries',
+        'sld': 'polygon'
+    },
+    {
+        'name': 'US County Boundaries',
+        'url': 'https://hifld-dhs-gii.opendata.arcgis.com/datasets/53270fe7036f428fbfb11c4511994a6f_0.geojson',
+        'table': 'us_county_boundaries',
+        'sld': 'polygon'
+    },
+    {
+        'name': 'US Urban Areas',
+        'url': 'https://hifld-dhs-gii.opendata.arcgis.com/datasets/12076211b9f047aca1153cb079bbd923_0.geojson',
+        'table': 'us_urban_areas',
+        'sld': 'polygon'
+    },
+    {
+        'name': 'Poultry Slaughtering and Processing Facilities ',
+        'url': 'https://hifld-dhs-gii.opendata.arcgis.com/datasets/b6b9cc72fb58476d92056d5c7ed25f8b_0.geojson',
+        'table': 'poultry_facilities',
+        'sld': 'point'
+    },
+    {
+        'name': 'State Fairgrounds',
+        'url': 'https://hifld-dhs-gii.opendata.arcgis.com/datasets/7f35881c8860490ab6a76516978e9e15_0.geojson',
+        'table': 'state_fairgrounds',
+        'sld': 'point'
+    },
+    {
+        'name': 'EPA Emergency Response (ER) Toxic Substances Control Act (TSCA) Facilities',
+        'url': 'https://hifld-dhs-gii.opendata.arcgis.com/datasets/5e2d8818b8ca4dbc8f14ea2e118b9a86_0.geojson',
+        'table': 'epa_tsca_facilities',
+        'sld': 'point'
+    }
+]
 
 def dataqs_extend():
     from settings import INSTALLED_APPS
